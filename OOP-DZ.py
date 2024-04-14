b9f1afe090d50cdb61cfaa3f36f3a5176c4c976b
Steve-b <stepan.babich@yandex.ru>
2024-04-14 21:45:55 +0600

    Add rate_lec and rate_hw

--- OOP-DZ.py
+++ OOP-DZ.py
@@ -1,3 +1,24 @@
+    # Оценка работы лекторов
+    def rate_lec(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached and 0 < grade <= 10:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    # Выведение основной информации о студенте
+    def __str__(self):
+        self.avg()
+        res = (f'Имя: {self.name}\n'
+               f'Фамилия: {self.surname}\n'
+               f'Средняя оценка за домашние задания: {self.avg_grades}\n'
+               f'Курсы в процессе изучения: 'f'{", ".join(self.courses_in_progress)}\n'
+               f'Завершенные курсы: {", ".join(self.finished_courses)}')
+        return res
+
+
 # Класс Mentor (лекторы и эксперты)
 class Mentor:
     def __init__(self, name, surname):
